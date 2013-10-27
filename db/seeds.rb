bc7ab9ce3b1320959fefcf571192666b1d4003bd
Ryan Malecky <rmalecky@gmail.com>
2013-10-27 16:57:16 -0400

    sets up seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Station.load_GTFS_stops(Rails.root.join("spec", "models", "stops_test.txt"))
